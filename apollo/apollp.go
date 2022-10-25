9ec98b5942c14f54ae5903aeb365fb5fb6f78843
hly <529416629@qq.com>
2022-10-25 14:52:29 +0800

    add apollo

--- apollo/apollp.go
+++ apollo/apollp.go
@@ -1,1 +1,32 @@
-package apollo
+package main
+
+import (
+	"fmt"
+	"github.com/philchia/agollo/v4"
+	"time"
+)
+
+func main() {
+	apollo := agollo.NewClient(&agollo.Conf{
+		AppID:          "123456",
+		Cluster:        "default",
+		NameSpaceNames: []string{"application.properties"},
+		MetaAddr:       "http://localhost:8080",
+	})
+	apollo.Start()
+	apollo.OnUpdate(func(event *agollo.ChangeEvent) {
+		for k, v := range event.Changes {
+			fmt.Println(fmt.Sprintf("-------%s-------!!!!", k))
+			switch v.ChangeType {
+			case agollo.ADD:
+				fmt.Println("-------add-------!!!!")
+			case agollo.MODIFY:
+				fmt.Println("-------modify-------!!!!")
+			case agollo.DELETE:
+				fmt.Println("-------delete-------!!!!")
+			}
+		}
+	})
+	fmt.Println(apollo.GetString("test_key"))
+	time.Sleep(100 * time.Second)
+}
--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.23.0
 	golang.org/x/net v0.1.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sync v0.1.0
 	google.golang.org/genproto v0.0.0-20221018160656-63c7b68cfc55 // indirect
 	google.golang.org/grpc v1.50.1
 	google.golang.org/protobuf v1.28.1
