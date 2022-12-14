adcda316348a21d535bd5dd84210fb3f07b64cdf
hly <529416629@qq.com>
2022-12-14 14:40:41 +0800

    redis hash

--- redis/redis.go
+++ redis/redis.go
@@ -4,16 +4,24 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"strconv"
 )
 
+var a map[int]int
+
 func main() {
-	ctx := context.Background()
+	//ctx := context.Background()
 	client := redis.NewClient(
 		&redis.Options{
 			Addr: ":6379",
 		})
 	//client.SetNX(ctx, "aa", "fwfwfwefwe", 1*time.Hour)
-	err := client.Get(ctx, "aaa").Err()
-	fmt.Println(err)
+	//err := client.Get(ctx, "aaa").Err()
 	//fmt.Println(err)
+	//fmt.Println(err)
+
+	err := client.HIncrBy(context.Background(), "test0101", strconv.FormatUint(uint64(101), 10), 1).Err()
+	fmt.Println(err)
+	fmt.Println(client.HGet(context.Background(), "test0101", strconv.FormatUint(uint64(101), 10)).Int())
+
 }
