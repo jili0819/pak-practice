91f34784be0346f4dceaf3d4627e77c1dacb4e49
hly <529416629@qq.com>
2022-12-26 18:40:33 +0800

    feat：add 模式

--- design_model/创建型模式/工厂模式.go
+++ design_model/创建型模式/工厂模式.go
@@ -1,49 +1,95 @@
 package main
 
-import "reflect"
+import "fmt"
 
-// ApiInter 简单工厂模式
-const (
-	SpeakImplsName   = "SpeakImpls"
-	WrangleImplsName = "WrangleImpls"
-)
+// Pancake 煎饼信息
+type Pancake interface {
+	// ShowFlour 煎饼使用的面粉
+	ShowFlour() string
+	// Value 煎饼价格
+	Value() float32
+}
 
-type (
-	ApiInter interface {
-		Say(string) string
-		Name() string
+// PancakeCook 煎饼厨师
+type PancakeCook interface {
+	// MakePancake 摊煎饼
+	MakePancake() Pancake
+}
+
+// PancakeVendor 煎饼小贩
+type PancakeVendor struct {
+	PancakeCook
+}
+
+// NewPancakeVendor ...
+func NewPancakeVendor(cook PancakeCook) *PancakeVendor {
+	return &PancakeVendor{
+		PancakeCook: cook,
 	}
-	SpeakImpls   struct{}
-	WrangleImpls struct{}
-)
-
-func (s *SpeakImpls) Say(msg string) string {
-	return msg
 }
 
-func (s *SpeakImpls) Name() string {
-	return SpeakImplsName
+// SellPancake 卖煎饼，先摊煎饼，再卖
+func (vendor *PancakeVendor) SellPancake() (money float32) {
+	return vendor.MakePancake().Value()
 }
 
-func (a *WrangleImpls) Say(msg string) string {
-	return msg
+// cornPancake 玉米面煎饼
+type cornPancake struct{}
+
+// NewCornPancake ...
+func NewCornPancake() *cornPancake {
+	return &cornPancake{}
 }
 
-func (a *WrangleImpls) Name() string {
-	return WrangleImplsName
+func (cake *cornPancake) ShowFlour() string {
+	return "玉米面"
 }
 
-func NewApiImpls(facType interface{}) ApiInter {
-	name := reflect.TypeOf(facType).Name()
-	switch name {
-	case SpeakImplsName:
-		return &SpeakImpls{}
-	case WrangleImplsName:
-		return &WrangleImpls{}
-	}
-	return &WrangleImpls{}
+func (cake *cornPancake) Value() float32 {
+	return 5.0
+}
+
+// milletPancake 小米面煎饼
+type milletPancake struct{}
+
+func NewMilletPancake() *milletPancake {
+	return &milletPancake{}
+}
+
+func (cake *milletPancake) ShowFlour() string {
+	return "小米面"
+}
+
+func (cake *milletPancake) Value() float32 {
+	return 8.0
+}
+
+// cornPancakeCook 制作玉米面煎饼厨师
+type cornPancakeCook struct{}
+
+func NewCornPancakeCook() *cornPancakeCook {
+	return &cornPancakeCook{}
+}
+
+func (cook *cornPancakeCook) MakePancake() Pancake {
+	return NewCornPancake()
+}
+
+// milletPancakeCook 制作小米面煎饼厨师
+type milletPancakeCook struct{}
+
+func NewMilletPancakeCook() *milletPancakeCook {
+	return &milletPancakeCook{}
+}
+
+func (cook *milletPancakeCook) MakePancake() Pancake {
+	return NewMilletPancake()
 }
 
 func main() {
-	NewApiImpls(new(SpeakImpls))
+	pancakeVendor := NewPancakeVendor(NewCornPancakeCook())
+	fmt.Printf("Corn pancake value is %v\n", pancakeVendor.SellPancake())
+
+	pancakeVendor = NewPancakeVendor(NewMilletPancakeCook())
+	fmt.Printf("Millet pancake value is %v\n", pancakeVendor.SellPancake())
 }
