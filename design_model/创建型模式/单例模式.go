f43a7caa7d6648ebf59256e0e4b2e447420f51a0
hly <529416629@qq.com>
2022-12-27 11:16:49 +0800

    feat：add 模式

--- design_model/创建型模式/单例模式.go
+++ design_model/创建型模式/单例模式.go
@@ -17,10 +17,12 @@ var ins *singleton
 var once sync.Once
 
 func GetIns() *singleton {
-	// 懒汉式单例模式
-	once.Do(func() {
-		ins = &singleton{}
-	})
+	if ins == nil {
+		// 懒汉式单例模式
+		once.Do(func() {
+			ins = &singleton{}
+		})
+	}
 	// 饿汉式模式
 	// 直接方法之外，全局初始化
 	return ins
