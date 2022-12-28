a1d4ddbff54bc31a643345a4bb4eb5d74a25e22a
hly <529416629@qq.com>
2022-12-28 10:34:47 +0800

    fix：

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/shopspring/decimal"
+	"time"
 )
 
 const a = 1000
 
 func main() {
-
-	//var a time.Time
-
-	b := int(decimal.NewFromFloat32(9.9).Mul(decimal.NewFromFloat32(100)).IntPart())
-	fmt.Println(b)
+	fmt.Println(time.Now().Clock())
 }
