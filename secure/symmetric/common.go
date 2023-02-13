1b8fc11c7046ff5a4cbcd6c82b786f29253cdba6
hly <529416629@qq.com>
2023-02-13 16:21:16 +0800

    secure 加密/解密

--- secure/symmetric/common.go
+++ secure/symmetric/common.go
@@ -7,18 +7,41 @@ import (
 	"encoding/base64"
 )
 
+// 注：
+// aes加密，分组长度128比特，密钥长度以32比特为单位在128比特-256比特之间选择，默认规格中只有128，192，256，
+// go提供的接口中，密钥长度只能为16字节
+// ecb/ctr:不需要初始化向量（go接口中的iv可以理解为随机数种子, iv的长度 == 明文分组的长度）
+// cbc/cfb/ofb:需要初始化向量（des/3des->8字节，aes->16字节，加解密向量相同）
+// 如果使用ecb/cbc分组模式需要对明文分组进行填充,cfb/ofb/ctr都不需要对分组明文填充
+
+// 密文先分组，再加密
+// des/3des按8字节分组
+// 分组密码模式(ecb/cbc模式需要填充 )
+
+// 1、创建des/3des/aes密码接口
+// crypto/des   (des/3des)
+// crypto/aes   (aes)
+// NewCipher(key []byte)(cipher.Block,error)
+
+// 2、如果使用cbc/ecb分组模式需要对密文分组进行填充,cfb/ofb/ctr都不需要对分组密文填充
+
+// 3、创建分组模式接口对象
+// cbc:NewCBCEncrypter(b block,iv []byte) BlockMode 加密
+// cfb:NewCFBEncrypter(b Block, iv []byte) Stream 加密
+
 func AesEncrypt(orig string, key string) string {
 	// 转成字节数组
 	origData := []byte(orig)
 	k := []byte(key)
 	// 分组秘钥
 	block, _ := aes.NewCipher(k)
+	// block, _ := des.NewCipher(k)
 	// 获取秘钥块的长度
 	blockSize := block.BlockSize()
+	// 加密模式 初始化向量默认为一个字节数组的block.BlockSize()长度
+	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
 	// 补全码
 	origData = PKCS7Padding(origData, blockSize)
-	// 加密模式
-	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
 	// 创建数组
 	cryted := make([]byte, len(origData))
 	// 加密
@@ -34,11 +57,14 @@ func AesDecrypt(cryted string, key string) string {
 	block, _ := aes.NewCipher(k)
 	// 获取秘钥块的长度
 	blockSize := block.BlockSize()
-	// 加密模式
+	// 加密模式，初始化向量默认为一个字节数组的block.BlockSize()长度
+	//blockMode := cipher.NewCFBDecrypter(block, k[:blockSize])
+	//blockMode := cipher.NewOFB(block, k[:blockSize])
 	blockMode := cipher.NewCBCDecrypter(block, k[:blockSize])
 	// 创建数组
 	orig := make([]byte, len(crytedByte))
 	// 解密
+	//blockMode.XORKeyStream(orig, crytedByte)
 	blockMode.CryptBlocks(orig, crytedByte)
 	// 去补全码
 	orig = PKCS7UnPadding(orig)
