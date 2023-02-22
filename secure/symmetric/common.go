265d5151ec027f24ffa09995f85b7d44675cff2c
hly <529416629@qq.com>
2023-02-22 10:25:43 +0800

    secure 加密/解密

--- secure/symmetric/common.go
+++ secure/symmetric/common.go
@@ -4,7 +4,12 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
+	"errors"
 )
 
 // 注：
@@ -69,6 +74,94 @@ func AesDecrypt(cryted string, key string) string {
 	// 去补全码
 	orig = PKCS7UnPadding(orig)
 	return string(orig)
+}
+
+// 私钥生成
+// openssl genrsa -out rsa_private_key.pem 1024
+var privateKey = []byte(`
+-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgQDcGsUIIAINHfRTdMmgGwLrjzfMNSrtgIf4EGsNaYwmC1GjF/bM
+h0Mcm10oLhNrKNYCTTQVGGIxuc5heKd1gOzb7bdTnCDPPZ7oV7p1B9Pud+6zPaco
+qDz2M24vHFWYY2FbIIJh8fHhKcfXNXOLovdVBE7Zy682X1+R1lRK8D+vmQIDAQAB
+AoGAeWAZvz1HZExca5k/hpbeqV+0+VtobMgwMs96+U53BpO/VRzl8Cu3CpNyb7HY
+64L9YQ+J5QgpPhqkgIO0dMu/0RIXsmhvr2gcxmKObcqT3JQ6S4rjHTln49I2sYTz
+7JEH4TcplKjSjHyq5MhHfA+CV2/AB2BO6G8limu7SheXuvECQQDwOpZrZDeTOOBk
+z1vercawd+J9ll/FZYttnrWYTI1sSF1sNfZ7dUXPyYPQFZ0LQ1bhZGmWBZ6a6wd9
+R+PKlmJvAkEA6o32c/WEXxW2zeh18sOO4wqUiBYq3L3hFObhcsUAY8jfykQefW8q
+yPuuL02jLIajFWd0itjvIrzWnVmoUuXydwJAXGLrvllIVkIlah+lATprkypH3Gyc
+YFnxCTNkOzIVoXMjGp6WMFylgIfLPZdSUiaPnxby1FNM7987fh7Lp/m12QJAK9iL
+2JNtwkSR3p305oOuAz0oFORn8MnB+KFMRaMT9pNHWk0vke0lB1sc7ZTKyvkEJW0o
+eQgic9DvIYzwDUcU8wJAIkKROzuzLi9AvLnLUrSdI6998lmeYO9x7pwZPukz3era
+zncjRK3pbVkv0KrKfczuJiRlZ7dUzVO0b6QJr8TRAA==
+-----END RSA PRIVATE KEY-----
+`)
+
+// 公钥: 根据私钥生成
+// openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
+var publicKey = []byte(`
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDcGsUIIAINHfRTdMmgGwLrjzfM
+NSrtgIf4EGsNaYwmC1GjF/bMh0Mcm10oLhNrKNYCTTQVGGIxuc5heKd1gOzb7bdT
+nCDPPZ7oV7p1B9Pud+6zPacoqDz2M24vHFWYY2FbIIJh8fHhKcfXNXOLovdVBE7Z
+y682X1+R1lRK8D+vmQIDAQAB
+-----END PUBLIC KEY-----
+`)
+
+func RsaSign(origData string) (sign string) {
+
+	return
+}
+
+// 加密
+func RsaEncrypt(origData string) (codeStr string, err error) {
+	//解密pem格式的公钥
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		err = errors.New("public key error")
+		return
+	}
+	// 解析公钥
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	// 类型断言
+	pub := pubInterface.(*rsa.PublicKey)
+	//加密
+	codeByte := make([]byte, 0)
+	codeByte, err = rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(origData))
+	if err != nil {
+		return
+	}
+	return base64.StdEncoding.EncodeToString(codeByte), nil
+}
+
+// 解密
+func RsaDecrypt(ciphertext string) (codeStr string, err error) {
+	cipherByte := make([]byte, 0)
+	cipherByte, err = base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return
+	}
+	//解密
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		err = errors.New("private key error!")
+		return
+	}
+	//解析PKCS1格式的私钥
+	private, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	// 解密
+	codeByte := make([]byte, 0)
+	codeByte, err = rsa.DecryptPKCS1v15(rand.Reader, private, cipherByte)
+	if err != nil {
+		return
+	}
+	codeStr = string(codeByte)
+	return
 }
 
 func ZeroPadding(ciphertext []byte, blockSize int) []byte {
--- secure/symmetric/common_test.go
+++ secure/symmetric/common_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	orig = "hello world"
+	orig = "hello worldfenwakngekwanglewaglawnkgawle325253"
 	key  = "123456781234567812345678"
 )
 
@@ -22,4 +22,18 @@ func TestAesDecrypt(t *testing.T) {
 	fmt.Println("密文：", encryptCode)
 	DecryptCode := AesDecrypt(encryptCode, key)
 	fmt.Println("原文：", DecryptCode)
+}
+
+func TestRsaEncrypt(t *testing.T) {
+	fmt.Println("原文：", orig)
+	encryptCode, err := RsaEncrypt(orig)
+	if err != nil {
+		return
+	}
+	fmt.Println("密文：", string(encryptCode))
+	DecryptCode, err := RsaDecrypt(encryptCode)
+	if err != nil {
+		return
+	}
+	fmt.Println("原文：", string(DecryptCode))
 }
